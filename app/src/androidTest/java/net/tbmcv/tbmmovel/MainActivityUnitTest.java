5cae090954fe0771e1da98e6805edfd695484f91
Daniel Getz <tanketz@gmail.com>
2015-06-14 15:41:42 -0400

    MainActivity: load and display saldo

--- app/src/androidTest/java/net/tbmcv/tbmmovel/MainActivityUnitTest.java
+++ app/src/androidTest/java/net/tbmcv/tbmmovel/MainActivityUnitTest.java
@@ -2,6 +2,16 @@ package net.tbmcv.tbmmovel;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
 
 public class MainActivityUnitTest extends BaseActivityUnitTest<MainActivity> {
     public MainActivityUnitTest() {
@@ -20,19 +30,87 @@ public class MainActivityUnitTest extends BaseActivityUnitTest<MainActivity> {
         assertTrue("Finish not called", isFinishCalled());
     }
 
-    public void testNoActivitySwitchIfConfigured() {
+    private void setStoredAcct(String acctName, String pw) {
         Context context = getInstrumentation().getTargetContext();
-        SharedPreferences prefs = context.getSharedPreferences(
-                context.getString(R.string.settings_key), Context.MODE_PRIVATE);
-        prefs.edit()
+        context.getSharedPreferences(context.getString(R.string.settings_key), Context.MODE_PRIVATE)
+                .edit()
                 .clear()
-                .putString(context.getString(R.string.setting_acctname), "c/9345678")
-                .putString(context.getString(R.string.setting_password), "blah")
+                .putString(context.getString(R.string.setting_acctname), acctName)
+                .putString(context.getString(R.string.setting_password), pw)
                 .commit();
+    }
+
+    public void testNoActivitySwitchIfConfigured() {
+        setStoredAcct("c/9345678", "blah");
         launch();
         getInstrumentation().callActivityOnResume(getActivity());
         getInstrumentation().waitForIdleSync();
         assertNull("Another Activity started", getStartedActivityIntent());
         assertFalse("Finish called", isFinishCalled());
+    }
+
+    public void testSaldoRequestSent() {
+        String acctName = "c/5050505";
+        String pw = "anything";
+        setStoredAcct(acctName, pw);
+        launch();
+        getInstrumentation().callActivityOnResume(getActivity());
+        getInstrumentation().waitForIdleSync();
+        JsonRestClient restClient = getRestClient();
+        InOrder inOrder = inOrder(restClient);
+        inOrder.verify(restClient).setAuth(acctName, pw);
+        inOrder.verify(restClient).fetch(
+                eq("GET"),
+                eq(Uri.parse("/idens/" + acctName + "/saldo/")),
+                isNull(JSONObject.class),
+                any(JsonRestClient.Callback.class));
+    }
+
+    protected void checkSaldoResponseDisplayed(int saldo, String formattedSaldo) {
+        setStoredAcct("c/9995555", "sosecret");
+        launch();
+        getInstrumentation().callActivityOnResume(getActivity());
+        getInstrumentation().waitForIdleSync();
+        ArgumentCaptor<JsonRestClient.Callback> callbackCaptor =
+                ArgumentCaptor.forClass(JsonRestClient.Callback.class);
+        verify(getRestClient()).fetch(
+                anyString(),
+                any(Uri.class),
+                any(JSONObject.class),
+                callbackCaptor.capture());
+        JSONObject result;
+        try {
+            result = new JSONObject().put("saldo", saldo);
+        } catch (JSONException e) {
+            throw new Error(e);
+        }
+        callbackCaptor.getValue().onSuccess(result);
+        getInstrumentation().waitForIdleSync();
+        TextView creditView = (TextView) getActivity().findViewById(R.id.creditValue);
+        assertEquals(formattedSaldo, creditView.getText().toString().trim());
+    }
+
+    public void testSaldoResponseDisplayedZero() {
+        checkSaldoResponseDisplayed(0, "0$00");
+    }
+
+    public void testSaldoResponseDisplayedSmall() {
+        checkSaldoResponseDisplayed(17, "17$00");
+    }
+
+    public void testSaldoResponseDisplayedLarge() {
+        checkSaldoResponseDisplayed(1234, "1.234$00");
+    }
+
+    public void testSaldoResponseDisplayedHuge() {
+        checkSaldoResponseDisplayed(2147483647, "2.147.483.647$00");
+    }
+
+    public void testSaldoResponseDisplayedNegativeSmall() {
+        checkSaldoResponseDisplayed(-3, "-3$00");
+    }
+
+    public void testSaldoResponseDisplayedNegativeLarge() {
+        checkSaldoResponseDisplayed(-99876, "-99.876$00");
     }
 }
--- app/src/main/java/net/tbmcv/tbmmovel/MainActivity.java
+++ app/src/main/java/net/tbmcv/tbmmovel/MainActivity.java
@@ -3,12 +3,26 @@ package net.tbmcv.tbmmovel;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
 
 public class MainActivity extends BaseActivity {
+    private NumberFormat creditFormat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        DecimalFormatSymbols formatSymbols = new DecimalFormatSymbols();
+        formatSymbols.setGroupingSeparator('.');
+        creditFormat = new DecimalFormat("#,##0'$00'", formatSymbols);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -22,12 +36,51 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        loadCredit();
+    }
+
+    protected void onNotConfigured() {
+        startActivity(new Intent(MainActivity.this, InitConfigActivity.class));
+        finish();
+    }
+
+    protected void setCredit(int credit) {
+        ((TextView) findViewById(R.id.creditValue)).setText(creditFormat.format(credit));
+    }
+
+    protected void loadCredit() {
         SharedPreferences config = getSharedPreferences(
                 getString(R.string.settings_key), Context.MODE_PRIVATE);
-        if (!(config.contains(getString(R.string.setting_acctname))
-                && config.contains(getString(R.string.setting_password)))) {
-            startActivity(new Intent(MainActivity.this, InitConfigActivity.class));
-            finish();
+        String acctName = config.getString(getString(R.string.setting_acctname), null);
+        String password = config.getString(getString(R.string.setting_password), null);
+        if (acctName == null || password == null) {
+            onNotConfigured();
+            return;
         }
+        final JsonRestClient restClient = getRestClient();
+        restClient.setAuth(acctName, password);
+        restClient.fetch("GET", Uri.parse("/idens/" + acctName + "/saldo/"),
+                null, new JsonRestClient.Callback() {
+            @Override
+            public void onSuccess(JSONObject result) {
+                final int credit;
+                try {
+                    credit = result.getInt("saldo");
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);  // TODO handle nicely
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setCredit(credit);
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Exception err) {
+                throw new RuntimeException(err);  // TODO if wrong auth, call onNotConfigured()
+            }
+        });
     }
 }
